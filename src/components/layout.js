a32178fb95cc4a4a03dada315b22319df05c8779
Daniel Coverdale <info@wetheterrors.com>
2020-05-04 19:53:27 +0100

    Added initial animation styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -43,15 +43,15 @@ const Layout = ({ children }) => (
         <div class="animation-container">
         <div class="flex">
           <div id ="animation1" class="w-2/5 dan-blue flex-square"></div>
-          <div class="w-1/5 dan-blue flex-square"></div>
+          <div id="animation2" class="w-1/5 dan-blue flex-square"></div>
           <div id="animation3" class="w-1/5 dan-orange flex-square"></div>
           <div id="animation4" class="w-1/5 dan-white flex-square"></div>
         </div>
         <div class="flex">
-          <div class="w-1/5 dan-blue flex-square"></div>
-          <div id ="animation6" class="w-1/5 dan-white flex-square"></div>
-          <div id ="animation7" class="w-1/5 dan-black flex-square"></div>
-          <div id ="animation8" class="w-2/5 dan-white flex-square"></div>
+          <div id="animation5" class="w-1/5 dan-blue flex-square"></div>
+          <div id="animation6" class="w-1/5 dan-white flex-square"></div>
+          <div id="animation7" class="w-1/5 dan-black flex-square"></div>
+          <div id="animation8" class="w-2/5 dan-white flex-square"></div>
         </div>
         </div>
         </div>
